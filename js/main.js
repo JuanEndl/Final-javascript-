7bf791b95de9824eb47f38fb46ac06e1fb30c368
juan Cruz <juanendl2021@gmail.com>
2021-10-13 09:55:26 -0300

    second commit

--- js/main.js
+++ js/main.js
@@ -1,12 +1,5 @@
 
 
-
-
-
-
-
-/////////////////////////////////////////////////////////////////////////////////////// metodo 1
-
 let acumulador = ``;
 let productos = [];
 
@@ -49,7 +42,7 @@ fetch('datos.json')
 
 
 
-$(".colorBlack").css("color", "black") // animacion y css desde javascript
+$(".colorBlack").css("color", "black") 
 
 
 
@@ -62,7 +55,7 @@ let productoCarrito =[];
 
 
 function agregarAlCarrito(id) {
-          //Si hay un localStorage creado, copialo en el arrego "productoCarrito"
+          
   if (localStorage.getItem('miCarritoDeCompras')){
   const carritoStoraje = localStorage.getItem('miCarritoDeCompras');
   productoCarrito = JSON.parse(carritoStoraje)
@@ -71,11 +64,11 @@ function agregarAlCarrito(id) {
   const productoEncontrado = productoCarrito.find(
     producto => producto.idProduc == id
   );
-          // El producto ya esta en carrito
+          
   if (productoEncontrado) {
     productoEncontrado.quantity += 1;
 
-          // El producto no esta en carrito
+          
   } else {
     const productoAGuardar = productos.find(p => p.idProduc == id);
     productoCarrito.push(productoAGuardar);
@@ -88,89 +81,3 @@ function agregarAlCarrito(id) {
  
 
 }
-
-
-
-
-
-
-
-
-
-
-
-////////////////////////////////////////////////////////////////
-
-// filtro
-
-
-
-
-/*let productoss = [];
-
-function filtrarMuebles(){
-  const nuevosProductos = productos.filter(producto => producto.category == "Joysticks") 
-  console.log(nuevosProductos)
-  let acumulador = ``;
-  
-  
-  fetch('datos.json')
-  .then(producto => producto.json())
-  //.then(data => console.log(data))
-  .then(data =>{
-    productos = data;
-    nuevosProductos.forEach((producto) => {
-      acumulador += `
-      <div class="card mb-4 ml-4 mr-4 mt-4 col-sm-12 col-md-5 col-lg-3" style="width: 18rem; id="${producto.idProduc}">
-        <img src="${producto.img}" class="card-img-top" alt="combo1">
-        <div class="card-body text-center card text-white bg-secondary">
-          <h5 class="card-title bg-dark">${producto.title}</h5>
-          <p class="card-text">${producto.description}</p>
-          <p>${producto.price}</p>
-          <button type="button" class=" butttton btn btn-primary" data-toggle="modal" data-target="#exampleModal" onclick="agregarAlCarrito('${producto.idProduc}')">
-          Comprar
-          </button>
-        </div>
-      </div>`;
-    });
-  
-    $("#cuadroProducto").append(acumulador).innerHTML ; // jQuery insertar DOM
-  
-  })
-}
-*/
-
-const carrito = productoCarrito ;
-
-const elementosMercadoPago = carrito.map(producto => {
-  return {
-    "title": producto.title,
-    "description": "",
-    "picture_url": "",
-    "category_id": producto.category,
-    "quantity": producto.quantity,
-    "currency_id": "ARS",
-    "unit_price": producto.price
-  }
-})
-
-
-
-
-
-const elemento = {"items": elementosMercadoPago }
-
-
-
-
-$.ajaxSetup({
-    headers:{
-      'Authorization':  'Bearer TEST-4487224230177816-101019-acb7579553ce501a4b81a7519b3ca72b-118910734',
-      'Content-Type': 'application/json'
-    }
-});
-
-
-$.post("https://api.mercadopago.com/checkout/preferences", JSON.stringify(elemento), function(respuesta, status) {
-  console.log(respuesta);   
-}); 
--- js/productootrapagina.js
+++ js/productootrapagina.js
@@ -26,22 +26,21 @@ carritoParseado.forEach((producto) => {
 
 function quitarCarrito(id) {
   let productosFiltrados = [];
-  // Busco el producto a borrar
+  
 
   const encontrarProducto = carritoParseado.find(
     producto => producto.idProduc == id
   ); 
 
-  // Elimino el Nodo de HTML siempre y cuando el localStorage tenga 1 producto y ese producto encontrado tenga una cantidad igual a 1
+  
   if (carritoParseado.length==1 && encontrarProducto.quantity==1){
   document.getElementById(encontrarProducto.idProduc).remove();
   } else {
-  // si no significa que quiero que se actualize la pagina para ver las cantidades correctamente.
+  
     location.reload();
   }
   
-// Si el producto encontrado para borrar es mas de 1, guardo el localstorage en producto filtrado
-// luego comparo del arreglo cada producto y si matchea con el que quiero borrar le resto 1 a cantidad
+
   if (encontrarProducto.quantity > 1){
     productosFiltrados = carritoParseado;
 
@@ -52,17 +51,17 @@ function quitarCarrito(id) {
     })
 
   } else {
-//ahora si el producto es unico (no es mas de uno) no lo necesito actualizar, filtro los demas (guardo en un nuevo 
-//arreglo "productosFiltrado" los productos que tengan un id diferente al que quiero borrar.
+
+
     productosFiltrados = carritoParseado.filter(idSearch => idSearch.idProduc != id);
   }  
   
-  // Si tengo un solo producto y el mismo tiene una unidad borro todo el localStorage
+  
   if (carritoParseado.length==1 && encontrarProducto.quantity==1){
     localStorage.removeItem("miCarritoDeCompras")
   }
   else {
-  //En caso contrario piso el localSotrage anterior con los productos filtrados que necesito.
+  
     localStorage.setItem(
       "miCarritoDeCompras",
       JSON.stringify(productosFiltrados))
